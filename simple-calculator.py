f665186183676d0dfc2d3950bb3d72042a4eef65
Jopee Zapido <jopeezapido@gmail.com>
2019-12-04 16:33:59 +0800

    First Upload

---
+++ simple-calculator.py
@@ -0,0 +1,11 @@
+price = int(input('Enter a price: '))
+quantity = int(input('Enter quantity: '))
+
+# improvements- only accept numbers, will return an error message and not accept if not a number
+
+result = (price * quantity)
+
+print('Price per quantity is: ' + str(result))
+
+
+
